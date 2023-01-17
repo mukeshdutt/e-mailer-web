ac6a951fc4d9e02b07f28a8263137b5bf7a76fdc
Mukesh Dutt <mukeshdutt5@gmail.com>
2023-01-17 23:22:52 +0530

    initial commit for web template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { HashRouter } from 'react-router-dom';
+import './assets/base.css';
+import Main from './DemoPages/Main';
+import configureStore from './config/configureStore';
+import { Provider } from 'react-redux';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = configureStore();
+const rootElement = document.getElementById('root');
+
+const renderApp = Component => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <Component />
+      </HashRouter>
+    </Provider>,
+    rootElement
+  );
+};
+
+renderApp(Main);
+
+if (module.hot) {
+  module.hot.accept('./DemoPages/Main', () => {
+    const NextApp = require('./DemoPages/Main').default;
+    renderApp(NextApp);
+  });
+}
+unregister();
+
+// registerServiceWorker();
+
